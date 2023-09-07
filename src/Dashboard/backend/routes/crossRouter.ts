8308f73da93e391ee5a7f8ef8967272f672f0971
Mohammad Kiyan <38220415+Kiyanof@users.noreply.github.com>
2023-09-07 03:46:36 -0700

    Load initiate value from database add

---
+++ src/Dashboard/backend/routes/crossRouter.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Router } from "express"
+import { addCross, getAllCross, setCrossByID } from "../controllers/crossController";
+
+const router:Router = express.Router()
+
+router.use((req: Request, res: Response, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+})
+
+router.get('/getAll', getAllCross)
+router.post('/set/:id', setCrossByID)
+router.post('/add', addCross)
+
+export default router
